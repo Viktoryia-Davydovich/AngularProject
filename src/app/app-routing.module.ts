3a066b390b355cd003a02266c443dd767aa0d271
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-07-18 16:54:15 +0300

    redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { CourselistComponent } from "./components/pages/courses/courselist/courselist.component";
 
-const routes: Routes = [{ path: "courses", component: CourselistComponent }];
+const routes: Routes = [
+  { path: "courses", component: CourselistComponent },
+  { path: "**", redirectTo: "/courses" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
