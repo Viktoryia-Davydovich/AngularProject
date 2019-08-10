df1eb7e33a96126b323e534a1a4931bc40716a77
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-08-11 00:56:46 +0300

    auth service mock

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  Login(username, password) {
+    const user = {
+      user: username,
+      password: password,
+      token: "fake-token"
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+  }
+
+  Logout() {
+    localStorage.removeItem("user");
+  }
+
+  IsAuthenticated() {
+    if (localStorage.getItem("user")) {
+      return true;
+    }
+    return false;
+  }
+
+  GetUserInfo() {
+    const user = localStorage.getItem("user");
+    return user;
+  }
 }
