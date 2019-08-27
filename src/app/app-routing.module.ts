b623c9846a1f927b71d01c2d98c9689160516a0d
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-08-27 18:33:28 +0300

    brcrmbs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AuthGuard } from "../auth/auth.guard";
 
 import { CourselistComponent } from "./components/pages/courses/courselist/courselist.component";
 import { LoginComponent } from "./components/pages/login/login.component";
@@ -7,10 +8,24 @@ import { AddCourseComponent } from "./components/pages/courses/addEditCourse/add
 import { NotFoundComponent } from "./components/pages/not-found/not-found.component";
 
 const routes: Routes = [
-  { path: "login", component: LoginComponent, data: {breadcrumb: "login"} },
-  { path: "courses/:id", component: AddCourseComponent, data: {breadcrumb: "edit course"}  },
-  { path: "courses/new", component: AddCourseComponent, data: {breadcrumb: "new course"}  },
-  { path: "courses", component: CourselistComponent, data: {breadcrumb: "courses"}  },
+  { path: "login", component: LoginComponent, data: { breadcrumb: "login" } },
+  {
+    path: "courses/:id",
+    component: AddCourseComponent,
+    data: { breadcrumb: "edit course" },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "courses/new",
+    component: AddCourseComponent,
+    data: { breadcrumb: "new course" },
+    canActivate: [AuthGuard]
+  },
+  {
+    path: "courses",
+    component: CourselistComponent,
+    data: { breadcrumb: "courses" }
+  },
   { path: "404", component: NotFoundComponent },
   { path: "**", redirectTo: "/courses" }
 ];
