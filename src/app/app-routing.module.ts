f98c692b25bd27300d5fc9fa8724a64e5989c8c2
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-08-27 18:40:57 +0300

    auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AuthGuard } from "../auth/auth.guard";
+import { AuthGuard } from "../app/auth/auth.guard";
 
 import { CourselistComponent } from "./components/pages/courses/courselist/courselist.component";
 import { LoginComponent } from "./components/pages/login/login.component";
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from "@angular/core";
   providedIn: "root"
 })
 export class AuthService {
+  redirectUrl: string;
+
   constructor() {}
 
   login(username: string, password: string) {
