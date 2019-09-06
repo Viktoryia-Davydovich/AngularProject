9b10c8d09182c9c0bb8d3a5ff57bbfb8f60bef66
Viktoryia Davydovich <Viktoryia.Davydovich@itechart-group.com>
2019-09-06 08:06:01 +0300

    added server

---
+++ Server/services/core/courses/courses.middleware.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const url = require("url");
+
+module.exports = server => {
+  router.get("/courses", (req, res, next) => {
+    let url_parts = url.parse(req.originalUrl, true),
+      query = url_parts.query,
+      from = query.start || 0,
+      to = +query.start + +query.count,
+      sort = query.sort,
+      queryStr = query.query,
+      courses = server.db.getState().courses;
+
+    if (!!query.textFragment) {
+      courses = courses.filter(
+        course =>
+          course.name
+            .concat(course.description)
+            .toUpperCase()
+            .indexOf(query.textFragment.toUpperCase()) >= 0
+      );
+    }
+
+    if (courses.length < to || !to) {
+      to = courses.length;
+    }
+    courses = courses.slice(from, to);
+
+    res.json(courses);
+  });
+
+  router.post("/courses/new", (req, res, next) => {
+    newCourse = req.body;
+    courses = server.db.getState().courses;
+    courses.push(newCourse);
+    courses.res.json(courses);
+  });
+
+  router.put("/courses/edit", (req, res, next) => {
+    let courses = server.db.getState().courses;
+    updCourse = courses.find(course => {
+      course.id === req.body.id;
+    });
+    updCourse.title = req.body.title;
+    updCourse.description = req.body.description;
+    updCourse.length = req.body.length;
+    updCourse.authors = req.body.authors;
+  });
+
+  router.delete("/courses/delete/:id", (req, res, next) => {
+    let courses = server.db.getState().courses;
+    deletedCourse = courses.find(course => {
+      course.id === req.body;
+    });
+
+    courses.splice(
+      this.courses.indexOf(
+        courses.find(course => {
+          course.id === req.params;
+        })
+      ),
+      1
+    );
+  });
+
+  return router;
+};
